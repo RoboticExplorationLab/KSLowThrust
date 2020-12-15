187baadcf0a14c9a2d5cc6530f49b4cf932b9d9b
Kevin Tracy <ktracy@stanford.edu>
2020-12-14 16:33:27 -0800

    working but taking a long time

---
+++ ks_functions.jl
@@ -0,0 +1,229 @@
+# There are two derrivatives of interest in the following functions, variables
+# differentiated with respect to time (t) are expressed as dot, and variables
+# differentiated with respect to ficticious time (s) are expressed as prime
+#
+# For a variable V:
+#   dV/dt = V_dot
+#   dV/ds = V_prime
+using StaticArrays
+using LinearAlgebra
+function L_fx(u)
+
+    return @SMatrix [u[1] -u[2] -u[3]  u[4];
+                     u[2]  u[1] -u[4] -u[3];
+                     u[3]  u[4]  u[1]  u[2];
+                     u[4] -u[3]  u[2] -u[1]]
+
+end
+
+function uprime_from_xdot(xdot,u)
+    # given a u and xdot, return u prime
+    # xdot = float(xdot)
+    # u = float(u)
+
+    u1 = u[1]
+    u2 = u[2]
+    u3 = u[3]
+    u4 = u[4]
+
+    xd1 = xdot[1]
+    xd2 = xdot[2]
+    xd3 = xdot[3]
+
+    up1 = (1/2)*( u1*xd1  +  u2*xd2  +  u3*xd3  )
+    up2 = (1/2)*(-u2*xd1  +  u1*xd2  +  u4*xd3  )
+    up3 = (1/2)*(-u3*xd1  -  u4*xd2  +  u1*xd3  )
+    up4 = (1/2)*( u4*xd1  -  u3*xd2  +  u2*xd3  )
+
+    uprime = [up1;up2;up3;up4]
+
+    return uprime
+
+    # return SVector((1/2)*( u[1]*xd[1]  +  u[2]*xd[2]  +  u[3]*xd3  ))
+
+end
+
+
+function u_from_x(x)
+    # get KS u from x, u1 is always 1
+    x = float(x)
+    r = norm(x)
+
+    u1 = .1
+    u4 = sqrt(.5*(r+x[1]) - u1^2)
+    u2 = (x[2]*u1 + x[3]*u4)/(r+x[1])
+    u3 = (x[3]*u1 - x[2]*u4)/(r+x[1])
+
+    u = [u1;u2;u3;u4]
+    return u
+end
+
+
+function x_from_u(u)
+    # gets x from u
+    # u = float(u)
+
+    return SVector( u[1]^2 - u[2]^2 - u[3]^2 + u[4]^2,
+                     2*(u[1]*u[2] - u[3]*u[4]),
+                     2*(u[1]*u[3] + u[2]*u[4]))
+
+end
+
+function xdot_from_u(u,u_prime)
+    up = u_prime;
+    u1 = u[1];u2 = u[2];u3 = u[3];u4 = u[4]
+    up1 = up[1];up2 = up[2];up3 = up[3];up4 = up[4]
+    r = dot(u,u)
+    x1d = (2/r)*(u1*up1 - u2*up2 - u3*up3 + u4*up4)
+    x2d = (2/r)*(u2*up1 + u1*up2 - u4*up3 - u3*up4)
+    x3d = (2/r)*(u3*up1 + u4*up2 + u1*up3 + u2*up4)
+
+    return [x1d;x2d;x3d]
+end
+
+function KS_ODE_J2!(state_prime,state,p,t)
+    # ODE for orbital motion that is with respect to ficticious time (s)
+    # state_prime is d(state)/ds
+    # state is ordered [u1 u2 u3 u4 u1' u2' u3' u4' h t]
+    # time in seconds since epoch will be denoted as 'tee' to avoid confusion
+    # with the ODE time variable
+
+    # scaling info
+    dscale = 1e6
+    tscale = 200
+
+    # unpack state
+    state = state[:]
+    u = state[1:4]
+    u_prime = state[5:8]
+    h = state[9]
+    #tee = state[10]
+    #r = state[11]
+
+    #J2 stuff
+    R_earth  = 6378137.0                   # m
+    mu = 3.986004418e14                    # m^3 s^{}-2}
+    x = x_from_u(u)*dscale                       # ECI, m
+    J2 =  1.081874e-3
+
+    # position vector in ECI from earth center to spacecraft
+    r_i = x[1]                             # ECI, m
+    r_j = x[2]                             # ECI, m
+    r_k = x[3]                             # ECI, m
+    R = norm(x)                          # ECI, m (dot(u,u)=norm(x))
+
+    # pertubation acceleration in ECI
+    P_j2 = (-1.5*(mu*J2*R_earth^2)/(R^4)) * [(r_i/R)*(1-3*r_k^2/R^2);(r_j/R)*(1-3*r_k^2/R^2);(r_k/R)*(3-3*r_k^2/R^2)]
+    P_j2 = P_j2*tscale^2/dscale
+    # Levi-Civita transformation matrix
+    L = L_fx(u)
+
+    # append 0 to end of P_j2 acceleration vector
+    P_j2 = [P_j2;0]
+
+    # no J2
+    #P_j2 = [0;0;0;0]
+
+    # ODE's
+    u_dp = -(h/2)*u + (R/(2*dscale))*L'*P_j2
+    #u_p = u_prime
+    tee_dot = dot(u,u)
+    h_dot = -2*dot(u_prime,L'*P_j2)
+
+    # index back into state_prime
+    state_prime[1:4] = u_prime
+    state_prime[5:8] = u_dp
+    state_prime[9] = h_dot
+    state_prime[10] = tee_dot
+    state_prime[11] = 2*u'*u_prime
+
+    return state_prime
+
+end
+
+function KS_ODE_J2_test!(ẋ,x,p,t)
+    # scaling info
+    dscale = 1e6
+    tscale = 200
+
+    # unpack state
+    x = x[:]
+    p = x[1:4]
+    p_prime = x[5:8]
+    h = x[9]
+    #tee = state[10]
+    #r = state[11]
+    #k = state[12]
+
+    #J2 stuff
+    R_earth  = 6378137.0                   # m
+    mu = 3.986004418e14                    # m^3 s^{}-2}
+    X = x_from_u(p)*dscale                       # ECI, m
+    J2 =  1.081874e-3
+
+    # position vector in ECI from earth center to spacecraft
+    r_i = X[1]                             # ECI, m
+    r_j = X[2]                             # ECI, m
+    r_k = X[3]                             # ECI, m
+    R = norm(X)                          # ECI, m (dot(u,u)=norm(x))
+
+    # pertubation acceleration in ECI
+    # P_j2 = (-1.5*(mu*J2*R_earth^2)/(R^4)) * [(r_i/R)*(1-3*r_k^2/R^2);(r_j/R)*(1-3*r_k^2/R^2);(r_k/R)*(3-3*r_k^2/R^2)]
+    # P_j2 = P_j2*tscale^2/dscale
+    # # Levi-Civita transformation matrix
+    L = L_fx(p)
+    #
+    # # append 0 to end of P_j2 acceleration vector
+    # P_j2 = [P_j2;0]
+
+    # no J2
+    P_j2 = [0;0;0;0]
+
+    # ODE's
+    p_dp = -(h/2)*p + (R/(2*dscale))*L'*P_j2
+    #u_p = p_prime
+    tee_dot = dot(p,p)
+    h_dot = -2*dot(p_prime,L'*P_j2)
+
+    # index back into state_prime
+    ẋ[1:4] = p_prime
+    ẋ[5:8] = p_dp
+    ẋ[9] = h_dot
+    ẋ[10] = tee_dot
+    ẋ[11] = 2*p'*p_prime
+    ẋ[12] = 2*(p_prime[1]*p[3] + p[1]*p_prime[3] + p_prime[2]*p[4] + p[2]*p_prime[4])
+    return ẋ
+end
+
+function OE2ECI(a,e,i,OMEGA,omega,nu)
+    # inputs in meters and radians for all
+
+    mu_earth = 3.986004418e14#m
+
+    #semi latus rectum
+    p = a*(1-e^2)
+
+    #calculate radius
+    r = p/(1+e*cos(nu))
+
+    #perifocal coorinates, page 73 of the 1971 book
+    r_PQ = [r*cos(nu); r*sin(nu);0]
+
+    v_PQ = (sqrt(mu_earth/p))*[-sin(nu); (e+cos(nu));0]
+
+    #now lets transform using a rotaion matrix
+
+    rx_i = [1 0 0; 0 cos(-i) sin(-i); 0 -sin(-i) cos(-i)]
+    rz_OMEGA = [cos(-OMEGA) sin(-OMEGA) 0 ; -sin(-OMEGA) cos(-OMEGA) 0; 0 0 1]
+    rz_omega = [cos(-omega) sin(-omega) 0 ; -sin(-omega) cos(-omega) 0; 0 0 1]
+
+    #now we transform
+    final_R = rz_OMEGA*rx_i*rz_omega
+
+    #now we get our final vectors
+    r_eci = final_R*r_PQ
+    v_eci = final_R*v_PQ
+
+    return r_eci, v_eci
+
+end
--- ksopt.jl
+++ ksopt.jl
@@ -72,7 +72,7 @@ end
 # scaling
 dscale = 1e7 # m
 tscale = 20000 # s
-uscale = 1000.0;
+uscale = 10000.0
 μ = 3.986004418e14 *(tscale^2)/(dscale^3)
 model = KSopt(μ,tscale, dscale, uscale)
 Base.size(::KSopt) = 12,3
@@ -90,8 +90,8 @@ t_0 = 0                                    # seconds
 x0 = SVector{12}([p_0; p_prime_0; h_0; t_0; norm(R_0); R_0[3]])
 
 # time stuff
-N = 1001
-dt = 3.0
+N = 2001
+dt = 8e-2
 tf = dt*(N-1)
 
 # cost function
@@ -102,9 +102,9 @@ tf = dt*(N-1)
 # Qf = 10*Q
 # obj = LQRObjective(Q, R, Qf, xf, N)
 scaling_num = 10
-r_Desired = 38e6/dscale#4.2#6.77814
-R_scale = 1
-Q = Diagonal(SVector{n}([1e-6*ones(10);scaling_num;100]))
+r_Desired = 42e6/dscale#4.2#6.77814
+R_scale = .1
+Q = Diagonal(SVector{n}([1e-12*ones(10);scaling_num;1000]))
 R = Diagonal(SVector(R_scale,R_scale,R_scale))
 q = SVector{n}([zeros(10);-r_Desired*scaling_num;0])
 costfun = QuadraticCost(Q,R,q=q)
@@ -114,21 +114,22 @@ obj = Objective(costfun, costfun_term, N)
 
 # constraints
 cons = ConstraintList(n,m,N-1)
-u_bnd = 5.0
+u_bnd = 5
 maxThrust = NormConstraint(n, m, u_bnd, TO.SecondOrderCone(), :control)
 TO.add_constraint!(cons, maxThrust, 1:N-1)
-
+# add_constraint!(cons, BoundConstraint(n,m, u_min=-1000, u_max=1000), 1:N-1)
 
 xf = randn(n)
 xf[11] = 4.2
 xf = SVector{n}(xf)
 prob = Problem(model, obj,xf, tf, x0=x0, constraints=cons,integration=RD.RK4)
 # prob = Problem(model, obj,xf*1000, tf, x0=x0,integration=RD.RK4)
-opts = SolverOptions(verbose=1,show_summary = true,iterations = 2000,
-projected_newton = false)
+opts = SolverOptions(verbose=2,show_summary = true,iterations = 20000,
+projected_newton = false, constraint_tolerance = 1e-4)
 solver = ALTROSolver(prob,opts)
 
 solve!(solver)
+
 
 X = states(solver)
 function mat_from_vec2(a)
@@ -152,7 +153,7 @@ mat"
 figure
 hold on
 plot3($r_eci_hist(1,:),$r_eci_hist(2,:),$r_eci_hist(3,:))
-%axis equal
+axis equal
 hold off
 "
 
@@ -166,17 +167,189 @@ hold off
 "
 
 
-# Q and R
-scaling_num = 10
-r_Desired = 42e6/dscale#4.2#6.77814
-# Q = Diagonal(1e-6*I,n)
-Q = Diagonal(SVector{n}([1e-6*ones(10);scaling_num;1]))
-# Q[11,11]=1*scaling_num
 
-# Q[12,12]= 1
-# Q = Diagonal(Q)
-#R = Diagonal(100000000000.0*I/uscale^2,m)
-R = Diagonal(SVector(1,1,1.0))
-# q = zeros(n)
-q = SVector{n}([zeros(10);-r_Desired*scaling_num;0])
-# q[11] = -r_Desired*scaling_num
+
+
+
+# using TrajectoryOptimization
+# using Altro
+# using RobotDynamics
+# using StaticArrays
+# using LinearAlgebra
+# const TO = TrajectoryOptimization
+# const RD = RobotDynamics
+#
+# function L_fx(u)
+#
+#     return @SMatrix [u[1] -u[2] -u[3]  u[4];
+#                      u[2]  u[1] -u[4] -u[3];
+#                      u[3]  u[4]  u[1]  u[2];
+#                      u[4] -u[3]  u[2] -u[1]]
+#
+# end
+#
+# struct KSopt <: TO.AbstractModel
+#     μ::Float64
+#     tscale::Float64
+#     dscale::Float64
+#     uscale::Float64
+# end
+# function RobotDynamics.dynamics(model::KSopt, x, u)
+#
+#     # unpack state
+#     p = SVector(x[1],x[2],x[3],x[4])
+#     p_prime = SVector(x[5],x[6],x[7],x[8])
+#     h = x[9]
+#
+#
+#     P_j2 = SVector(u[1],u[2],u[3])/model.uscale
+#     # Levi-Civita transformation matrix
+#     L = L_fx(p)
+#
+#     # append 0 to end of P_j2 acceleration vector
+#     P_j2 = SVector{4}([P_j2;0]*model.tscale^2/model.dscale)
+#
+#     p_dp = -(h/2)*p + ((dot(p,p))/(2))*L'*P_j2
+#
+#     return SVector(p_prime[1],p_prime[2],p_prime[3],p_prime[4],
+#                    p_dp[1],p_dp[2],p_dp[3],p_dp[4],
+#                    -2*dot(p_prime,L'*P_j2),
+#                    dot(p,p),
+#                    2*p'*p_prime,
+#                    2*(p_prime[1]*p[3] + p[1]*p_prime[3] + p_prime[2]*p[4] + p[2]*p_prime[4]))
+#
+# end
+# # function RobotDynamics.dynamics(model::KSopt, x, u)
+# #
+# #     # unpack state
+# #     p = SVector(x[1],x[2],x[3],x[4])
+# #     p_prime = SVector(x[5],x[6],x[7],x[8])
+# #     h = x[9]
+# #
+# #     # scale u
+# #     # u /= model.uscale
+# #
+# #     L = L_fx(p)
+# #     P_j2 = SVector(u[1],u[2],u[3],0)/model.uscale
+# #     p_dp = -(h/2)*p + ((dot(p,p))/(2*dscale))*L'*P_j2
+# #
+# #     return SVector(p_prime[1],p_prime[2],p_prime[3],p_prime[4],
+# #                    p_dp[1],p_dp[2],p_dp[3],p_dp[4],
+# #                    -2*dot(p_prime,L'*P_j2),
+# #                    dot(p,p),
+# #                    2*p'*p_prime,
+# #                    2*(p_prime[1]*p[3] + p[1]*p_prime[3] + p_prime[2]*p[4] + p[2]*p_prime[4]))
+# #
+# # end
+#
+# # scaling
+# dscale = 1e7 # m
+# tscale = 20000 # s
+# uscale = 1000.0;
+# μ = 3.986004418e14 *(tscale^2)/(dscale^3)
+# model = KSopt(μ,tscale, dscale, uscale)
+# Base.size(::KSopt) = 12,3
+# n,m = size(model)
+#
+# # initial conditions
+# r_eci = @SVector [6.578137000000001e6,0,0]
+# v_eci = @SVector [0,9111.210131355681,4642.393437617197]
+# R_0 = r_eci/dscale
+# V_0 = v_eci*tscale/dscale
+# p_0 = u_from_x(R_0)                          # u
+# p_prime_0 = uprime_from_xdot(V_0,p_0)     # du/ds
+# h_0 = μ/norm(R_0) - .5*norm(V_0)^2         # m^2/s^2
+# t_0 = 0                                    # seconds
+# x0 = SVector{12}([p_0; p_prime_0; h_0; t_0; norm(R_0); R_0[3]])
+#
+# # time stuff
+# N = 501
+# dt = 1.0
+# tf = dt*(N-1)
+#
+# # cost function
+# Q = 10000*Diagonal(SVector{n}([zeros(10);1;0]))
+# R = 1*Diagonal(@SVector fill(1.0, m))
+# r_Desired = 42e6/dscale
+# xf = SVector{n}([zeros(10);r_Desired;0])
+# Qf = 10*Q
+# obj = LQRObjective(Q, R, Qf, xf, N)
+# # scaling_num = 10
+# # r_Desired = 38e6/dscale#4.2#6.77814
+# # R_scale = 1
+# # Q = Diagonal(SVector{n}([1e-6*ones(10);scaling_num;100]))
+# # R = Diagonal(SVector(R_scale,R_scale,R_scale))
+# # q = SVector{n}([zeros(10);-r_Desired*scaling_num;0])
+# # costfun = QuadraticCost(Q,R,q=q)
+# # costfun_term = QuadraticCost(Q,R,q=q)
+# # obj = Objective(costfun, costfun_term, N)
+#
+#
+# # constraints
+# cons = ConstraintList(n,m,N-1)
+# u_bnd = 5.0
+# maxThrust = NormConstraint(n, m, u_bnd, TO.SecondOrderCone(), :control)
+# TO.add_constraint!(cons, maxThrust, 1:N-1)
+#
+#
+# # xf = randn(n)
+# # xf[11] = 4.2
+# # xf = SVector{n}(xf)
+# prob = Problem(model, obj,xf, tf, x0=x0, constraints=cons,integration=RD.RK4)
+# # prob = Problem(model, obj,xf*1000, tf, x0=x0,integration=RD.RK4)
+# opts = SolverOptions(verbose=2,show_summary = true,
+# projected_newton = false)
+# solver = ALTROSolver(prob,opts)
+#
+# solve!(solver)
+#
+# X = states(solver)
+# function mat_from_vec2(a)
+#     "Turn a vector of vectors into a matrix"
+#
+#
+#     rows = length(a[1])
+#     columns = length(a)
+#     A = zeros(rows,columns)
+#
+#     for i = 1:columns
+#         A[:,i] = a[i]
+#     end
+#
+#     return A
+# end
+# r_eci_hist = mat_from_vec2([x_from_u(X[i]) for i = 1:length(X)])
+#
+#
+# mat"
+# figure
+# hold on
+# plot3($r_eci_hist(1,:),$r_eci_hist(2,:),$r_eci_hist(3,:))
+# %axis equal
+# hold off
+# "
+#
+# U = mat_from_vec2(controls(solver))
+#
+# mat"
+# figure
+# hold on
+# plot($U')
+# hold off
+# "
+#
+#
+# # Q and R
+# scaling_num = 10
+# r_Desired = 42e6/dscale#4.2#6.77814
+# # Q = Diagonal(1e-6*I,n)
+# Q = Diagonal(SVector{n}([1e-6*ones(10);scaling_num;1]))
+# # Q[11,11]=1*scaling_num
+#
+# # Q[12,12]= 1
+# # Q = Diagonal(Q)
+# #R = Diagonal(100000000000.0*I/uscale^2,m)
+# R = Diagonal(SVector(1,1,1.0))
+# # q = zeros(n)
+# q = SVector{n}([zeros(10);-r_Desired*scaling_num;0])
+# # q[11] = -r_Desired*scaling_num
