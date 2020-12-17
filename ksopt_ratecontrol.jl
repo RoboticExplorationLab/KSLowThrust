2de0bfcad0ee720af95e91171bb20762b2485402
Kevin Tracy <ktracy@stanford.edu>
2020-12-16 23:23:35 -0800

    112 day working

--- ksopt_ratecontrol.jl
+++ ksopt_ratecontrol.jl
@@ -21,12 +21,15 @@ struct KSopt <: TO.AbstractModel
     dscale::Float64
     uscale::Float64
 end
-function RobotDynamics.dynamics(model::KSopt, x, u)
+function RobotDynamics.dynamics(model::KSopt, x, u_dot)
 
     # unpack state
     p = SVector(x[1],x[2],x[3],x[4])
     p_prime = SVector(x[5],x[6],x[7],x[8])
     h = x[9]
+    #r = x[10]
+    #z = x[11]
+    u = SVector(x[12],x[13],x[14])
 
 
     P_j2 = SVector(u[1],u[2],u[3])/model.uscale
@@ -42,7 +45,8 @@ function RobotDynamics.dynamics(model::KSopt, x, u)
                    p_dp[1],p_dp[2],p_dp[3],p_dp[4],
                    -2*dot(p_prime,L'*P_j2),
                    2*p'*p_prime,
-                   2*(p_prime[1]*p[3] + p[1]*p_prime[3] + p_prime[2]*p[4] + p[2]*p_prime[4]))
+                   2*(p_prime[1]*p[3] + p[1]*p_prime[3] + p_prime[2]*p[4] + p[2]*p_prime[4]),
+                   u_dot[1],u_dot[2],u_dot[3])
 
 end
 # function RobotDynamics.dynamics(model::KSopt, x, u)
@@ -74,7 +78,7 @@ tscale = 20000 # s
 uscale = 10000.0
 μ = 3.986004418e14 *(tscale^2)/(dscale^3)
 model = KSopt(μ,tscale, dscale, uscale)
-Base.size(::KSopt) = 11,3
+Base.size(::KSopt) = 14,3
 n,m = size(model)
 
 # initial conditions
@@ -86,11 +90,11 @@ p_0 = u_from_x(R_0)                          # u
 p_prime_0 = uprime_from_xdot(V_0,p_0)     # du/ds
 h_0 = μ/norm(R_0) - .5*norm(V_0)^2         # m^2/s^2
 t_0 = 0                                    # seconds
-x0 = SVector{11}([p_0; p_prime_0; h_0; norm(R_0); R_0[3]])
+x0 = SVector{n}([p_0; p_prime_0; h_0; norm(R_0); R_0[3];zeros(3)])
 
 # time stuff
-N = 2001
-dt = 2e-2
+N = 3501
+dt = 4e-2
 tf = dt*(N-1)
 
 # cost function
@@ -103,19 +107,19 @@ tf = dt*(N-1)
 scaling_num = 10
 r_Desired = 42e6/dscale#4.2#6.77814
 R_scale = .01
-Q = Diagonal(SVector{n}([0*ones(9);scaling_num;100]))
-R = Diagonal(SVector(R_scale,R_scale,R_scale))
-q = SVector{n}([zeros(9);-r_Desired*scaling_num;0])
+Q = Diagonal(SVector{n}([0*ones(9);scaling_num;100;R_scale*ones(3)]))
+R = 0.001*Diagonal(SVector(R_scale,R_scale,R_scale))
+q = SVector{n}([zeros(9);-r_Desired*scaling_num;0;zeros(3)])
 costfun = QuadraticCost(Q,R,q=q)
 costfun_term = QuadraticCost(Q,R,q=q)
 obj = Objective(costfun, costfun_term, N)
 
 
 # constraints
-cons = ConstraintList(n,m,N-1)
-u_bnd = 16
-maxThrust = NormConstraint(n, m, u_bnd, TO.SecondOrderCone(), :control)
-TO.add_constraint!(cons, maxThrust, 1:N-1)
+cons = ConstraintList(n,m,N)
+u_bnd = 3.2
+maxThrust = NormConstraint(n, m, u_bnd, TO.SecondOrderCone(), 12:14)
+TO.add_constraint!(cons, maxThrust, 1:N)
 # add_constraint!(cons, BoundConstraint(n,m, u_min=-1000, u_max=1000), 1:N-1)
 
 xf = randn(n)
@@ -123,6 +127,7 @@ xf[11] = 4.2
 xf = SVector{n}(xf)
 prob = Problem(model, obj,xf, tf, x0=x0, constraints=cons,integration=RD.RK4)
 # prob = Problem(model, obj,xf*1000, tf, x0=x0,integration=RD.RK4)
+# initial_controls!(prob,U_old)
 opts = SolverOptions(verbose=2,
                      show_summary = true,
                      iterations = 20000,
@@ -159,17 +164,18 @@ axis equal
 hold off
 "
 
-U = mat_from_vec2(controls(solver))
+Xm = mat_from_vec2(X)
+U_real = Xm[12:14,:]
 
 mat"
 figure
 hold on
-stairs($U')
+plot($U_real')
 hold off
 "
 
 
-U = controls(solver)
+U = [X[i][12:14] for i = 1:length(X)]
 Unorm = [norm(U[i]) for i = 1:length(U)]
 
 mat"
@@ -232,5 +238,7 @@ hold off
 
 # using JLD2
 # X = states(solver)
-# U = states(solver)
-# @save "saved_transfers/34_day_transfer.jld2" X U
+# U = controls(solver)
+# @save "rate_control_transfers/78_day_transfer.jld2" X U
+#
+# U_old = controls(solver)
